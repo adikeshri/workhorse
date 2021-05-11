03c79a76abfd16a54ef7eeb154899dc1757cbfbc
Aditya Keshri <31363648+adikeshri@users.noreply.github.com>
2021-05-11 23:58:58 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,9 +4,6 @@ from notify_run import Notify
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-
-
-
 while True:
     
     print("-"*50)
