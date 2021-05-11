34c3f09af09705293935ec6a0e472231b0b559a8
Aditya Keshri <31363648+adikeshri@users.noreply.github.com>
2021-05-11 23:58:44 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,18 +1,6 @@
 import requests
 import time
-
-
-
-
-
-
-
-
-
-
-
 from notify_run import Notify 
-
 
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
