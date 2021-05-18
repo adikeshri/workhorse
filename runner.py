3266c9a89c1a5b414c8d9d178ac26b1e7b3a1c48
Aditya Keshri <31363648+adikeshri@users.noreply.github.com>
2021-05-19 00:31:29 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,10 +5,8 @@ from notify_run import Notify
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 while True:
-    
     print("-"*50)
     print("Calling cowin api")
-    
     response=requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=97&date=10-05-2021",headers=headers)
     if response.status_code!=200:
         print("API call failed")
