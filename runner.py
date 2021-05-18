96bdfe4753fc291329cb923d093904e0da60876d
Aditya Keshri <31363648+adikeshri@users.noreply.github.com>
2021-05-19 00:31:58 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,7 @@
 import requests
 import time
 from notify_run import Notify 
-
 headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-
 while True:
     print("-"*50)
     print("Calling cowin api")
@@ -12,19 +10,12 @@ while True:
         print("API call failed")
         time.sleep(5)
         continue
-
     jsonResp=response.json()
-
-
     sessions=jsonResp["sessions"]
-
-
     availSessionsFor18 = []
-
     for session in sessions:
         if session["min_age_limit"]!=45:
             availSessionsFor18.append(session)
-
     if availSessionsFor18 !=[]:
         print("Slots available")
         print(availSessionsFor18)
